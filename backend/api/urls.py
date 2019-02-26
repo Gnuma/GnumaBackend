b5683b5894cd05603e97c75f8ad2cc4d39e4749a
ihategoto <cdemilio80@gmail.com>
2019-02-26 20:10:10 +0100

    DEBUG conditiona added

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('gnuma/v1/auth/', include('rest_auth.urls')), #login management
     path('gnuma/v1/auth/registration/', include('rest_auth.registration.urls')), #signup management
     path('gnuma/v1/', include('gnuma.urls'))
-] + static(settings.IMAGES_URL, document_root = settings.IMAGES_DIR)
+]
 
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
