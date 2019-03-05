ed7c42205dd7467046d4b59e69bdb279781a9afa
ihategoto <cdemilio80@gmail.com>
2019-03-05 19:47:32 +0100

    ImageAd added

--- backend/gnuma/admin.py
+++ backend/gnuma/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import  GnumaUser, Office, Class, Ad, Book, Queue_ads
+from .models import  GnumaUser, Office, Class, Ad, Book, Queue_ads, ImageAd
 
 admin.site.register(Ad)
 admin.site.register(Class)
@@ -7,3 +7,4 @@ admin.site.register(Office)
 admin.site.register(GnumaUser)
 admin.site.register(Book)
 admin.site.register(Queue_ads)
+admin.site.register(ImageAd)
