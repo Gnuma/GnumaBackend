f728d2272b2e5defe99a70b6e02055a414f5b677
ihategoto <cdemilio80@gmail.com>
2019-03-30 03:20:14 +0100

    ISINSTANCE IF DELETEDE

--- backend/gnuma/customFields.py
+++ backend/gnuma/customFields.py
@@ -7,30 +7,21 @@ class Base64ImageField(serializers.ImageField):
         import base64
         import six
         import uuid
+        print('IM HERE')
+        try:
+            decoded_file = base64.b64decode(data)
+        except TypeError:
+            print("DEBUG PRINT : THE SERIALIZER WAS NOT ABLE TO DECODE THE BODY")
+            self.fail('invalid_image')
 
-        # Check if this is a base64 string
-        if isinstance(data, six.string_types):
-            # Check if the base64 string is in the "data:" format
-            #if 'data:' in data and ';base64,' in data:
-                # Break out the header from the base64 content
-                #header, data = data.split(';base64,')
+        # Generate file name:
+        file_name = str(uuid.uuid4())[:12] # 12 characters are more than enough.
+        # Get the file name extension:
+        file_extension = self.get_file_extension(file_name, decoded_file)
 
-            # Try to decode the file. Return validation error if it fails.
-            print('IM HERE')
-            try:
-                decoded_file = base64.b64decode(data)
-            except TypeError:
-                print("DEBUG PRINT : THE SERIALIZER WAS NOT ABLE TO DECODE THE BODY")
-                self.fail('invalid_image')
+        complete_file_name = "%s.%s" % (file_name, file_extension, )
 
-            # Generate file name:
-            file_name = str(uuid.uuid4())[:12] # 12 characters are more than enough.
-            # Get the file name extension:
-            file_extension = self.get_file_extension(file_name, decoded_file)
-
-            complete_file_name = "%s.%s" % (file_name, file_extension, )
-
-            data = ContentFile(decoded_file, name=complete_file_name)
+        data = ContentFile(decoded_file, name=complete_file_name)
 
 
         return super(Base64ImageField, self).to_internal_value(data)
