4ca60154df50f86aa42441f40979058414488a9a
ihategoto <cdemilio80@gmail.com>
2019-03-30 01:34:54 +0100

    Multipart base64 supported

--- backend/gnuma/customFields.py
+++ backend/gnuma/customFields.py
@@ -3,23 +3,39 @@ from rest_framework import serializers
 class Base64ImageField(serializers.ImageField):
 
     def to_internal_value(self, data):
-        from django.core.files.uploadedfile import InMemoryUploadedFile
+        from django.core.files.base import ContentFile
         import base64
-        import io
-        """
-        Data is an instance of InMemoryUploadedFile class.
-        """
-        if isinstance(data, InMemoryUploadedFile):
-            data_decoded = b''
-            for byte in data.chunks():
-                try:
-                    data_decoded += base64.b64decode(byte, validate = True)
-                except Exception as e:
-                    print('EXCEPTION RAISED IN B64 DECODE: %s' % str(e))
-            
-        newFile = io.BytesIO(data_decoded)
-        newData = InMemoryUploadedFile(file = newFile, name = data.name, size = data.size, content_type = data.content_type, content_type_extra = data.content_type_extra, charset = data.charset, field_name = data.field_name)
-        #
-        # Now we got our original data decoded, so just let's run the DRF's validating process.
-        #
-        return super(Base64ImageField, self).to_internal_value(newData)
+        import six
+        import uuid
+
+        # Check if this is a base64 string
+        if isinstance(data, six.string_types):
+            # Check if the base64 string is in the "data:" format
+            if 'data:' in data and ';base64,' in data:
+                # Break out the header from the base64 content
+                header, data = data.split(';base64,')
+
+            # Try to decode the file. Return validation error if it fails.
+            try:
+                decoded_file = base64.b64decode(data)
+            except TypeError:
+                self.fail('invalid_image')
+
+            # Generate file name:
+            file_name = str(uuid.uuid4())[:12] # 12 characters are more than enough.
+            # Get the file name extension:
+            file_extension = self.get_file_extension(file_name, decoded_file)
+
+            complete_file_name = "%s.%s" % (file_name, file_extension, )
+
+            data = ContentFile(decoded_file, name=complete_file_name)
+
+        return super(Base64ImageField, self).to_internal_value(data)
+
+    def get_file_extension(self, file_name, decoded_file):
+        import imghdr
+
+        extension = imghdr.what(file_name, decoded_file)
+        extension = "jpg" if extension == "jpeg" else extension
+
+        return extension
